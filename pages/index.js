119b65c13f2b78223a176e8fb777fe7c1aebe147
tapia1013 <manbearpig1013@gmail.com>
2021-06-11 14:10:34 -0700

    finished user page

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,30 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <h1>
-      TEST
-    </h1>
+    <div>
+      <div className="login-user">
+        <div className="login-user__top">
+          <div className="login-user__logo" />
+          <span className="login-user__title">
+            Who is watching?
+          </span>
+        </div>
+
+        <div className="login-user__form">
+          <div className="login-user__user-box">
+            <img
+              className="login-user__user-img"
+              src="https://pbs.twimg.com/profile_images/378800000519722378/c0fd8906bcfeac191dac58c00e6dcda8.jpeg"
+            />
+            <div className="login-user__user-name">Cookie</div>
+          </div>
+        </div>
+
+        <div className="login-user__buttons">
+          <button className="login-user__adult">Add Adult</button>
+          <button className="login-user__kid">Add Kid</button>
+        </div>
+      </div>
+    </div>
   )
 }
