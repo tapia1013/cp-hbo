6c627b31897e7b7ad2c479a02d4c0867ace4c776
tapia1013 <manbearpig1013@gmail.com>
2021-06-14 17:48:48 -0700

    added template for header and file/folder structure

---
+++ components/UI/Header.js
@@ -0,0 +1,22 @@
+
+const Header = (props) => {
+  return (
+    <header className="top-header">
+      <div className="top-header__left-side">
+        <div className="top-header__menu-btn">
+          <i className="fas fa-bars" />
+        </div>
+        <div className="top-header__search-btn">
+          <i className="fas fa-search" />
+        </div>
+      </div>
+      <div className="top-header__logo"></div>
+      <div className="top-header__account">
+        <img className="top-header__user-img" src="https://pbs.twimg.com/profile_images/378800000519722378/c0fd8906bcfeac191dac58c00e6dcda8.jpeg" />
+        <div className="top-header__user-name">Cookie</div>
+      </div>
+    </header>
+  )
+}
+
+export default Header;
---
+++ pages/home.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import MainLayout from '../components/Layout/MainLayout';
+
+
+
+
+export default function HomeView() {
+  return (
+    <MainLayout>
+      <div>
+        HOME
+      </div>
+    </MainLayout>
+  )
+}
