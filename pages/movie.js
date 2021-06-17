7229b6eb5e749ebe879dcdfa5ea9bbbb0905db6f
tapia1013 <manbearpig1013@gmail.com>
2021-06-16 17:32:45 -0700

    completed the previous task, now adding Create Account Section

---
+++ pages/movie.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import MainLayout from '../components/Layout/MainLayout';
+import CastInfo from '../components/UI/CastInfo/CastInfo';
+import FeaturedMedia from '../components/UI/FeaturedMedia/FeaturedMedia';
+import PosterView from '../components/UI/PosterView/PosterView';
+
+
+
+export default function HomeView() {
+  return (
+    <MainLayout>
+      <FeaturedMedia />
+      <PosterView />
+      <CastInfo />
+    </MainLayout>
+  )
+}
