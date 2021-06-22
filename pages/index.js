e81aec7ec412e9f9e96732f6e835d7c514263fa7
tapia1013 <manbearpig1013@gmail.com>
2021-06-22 15:52:32 -0700

    added lazyload for images and currently attaching a skeleton to load with lazyload

--- pages/index.js
+++ pages/index.js
@@ -8,8 +8,8 @@ import FeaturedMedia from '../components/UI/FeaturedMedia/FeaturedMedia';
 // import PosterView from '../components/UI/PosterView/PosterView';
 import MediaRow from '../components/UI/MediaRow/MediaRow';
 import AuthCheck from '../components/AuthCheck';
+import LazyLoad from 'react-lazyload';
 
-// MAKING MEDIA ROW MORE DYNAMIC @16:38s
 
 export default function Home() {
   const globalState = useStateContext();
@@ -20,36 +20,48 @@ export default function Home() {
   return AuthCheck(
     <MainLayout>
       <FeaturedMedia />
+      <LazyLoad height={680} offset={-200} placeholder={<h1>Loading</h1>}>
       <MediaRow
         title="Movies"
         type="large-v"
         endpoint="discover/movie?sort_by=popularity.desc&primary_release_year=2021"
       />
+      </LazyLoad>
+      <LazyLoad height={332} offset={-200}>
       <MediaRow
         title="Series"
         type="small-h"
         endpoint="discover/tv?primary_release_year=2021"
       />
+      </LazyLoad>
+      <LazyLoad height={440} offset={-200}>
       <MediaRow
         title="Action"
         type="small-v"
         endpoint="discover/movie?with_genres=28&primary_release_year=2021"
       />
+      </LazyLoad>
+      <LazyLoad height={200}>
       <MediaRow
         title="Horror"
         type="large-v"
         endpoint="discover/movie?with_genres=27&primary_release_year=2021"
       />
+      </LazyLoad>
+      <LazyLoad height={200}>
       <MediaRow
         title="Animations"
         type="large-h"
         endpoint="discover/movie?with_genres=16&primary_release_year=2021"
       />
+      </LazyLoad>
+      <LazyLoad height={200}>
       <MediaRow
         title="Scifi"
         type="small-v"
         endpoint="discover/movie?with_genres=878&primary_release_year=2021"
       />
+      </LazyLoad>
     </MainLayout>
   )
 }
