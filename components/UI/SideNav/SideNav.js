53abc7534d5e1875cdcf6fae2dd07e778a862126
tapia1013 <manbearpig1013@gmail.com>
2021-06-24 18:37:58 -0700

    create a index media type page plus genre nav

--- components/UI/SideNav/SideNav.js
+++ components/UI/SideNav/SideNav.js
@@ -1,5 +1,5 @@
 import { useStateContext } from '../../HBOProvider';
-
+import Link from 'next/link';
 
 
 const SideNav = (props) => {
@@ -18,102 +18,26 @@ const SideNav = (props) => {
         <i className="fas fa-times" />
       </div>
       <ul className="side-nav__main">
-        <li>
-          <a href="/" className="active">
-            Home
-          </a>
+        <li onClick={() => globalState.setSideNavOpenAction(false)}>
+          <Link href="/">
+            <a>
+              Home
+            </a>
+          </Link>
         </li>
-        <li>
-          <a href="/" className="">
-            Series
-          </a>
+        <li onClick={() => globalState.setSideNavOpenAction(false)}>
+          <Link href="/movie">
+            <a>
+              Movies
+            </a>
+          </Link>
         </li>
-        <li>
-          <a href="/" className="">
-            Movies
-          </a>
-        </li>
-        <li>
-          <a href="/" className="">
-            Originals
-          </a>
-        </li>
-        <li>
-          <a href="/" className="">
-            Just Added
-          </a>
-        </li>
-        <li>
-          <a href="/" className="">
-            Last Chance
-          </a>
-        </li>
-        <li>
-          <a href="/" className="">
-            Coming Soon
-          </a>
-        </li>
-        <li>
-          <a href="/" className="">
-            Trending Now
-          </a>
-        </li>
-      </ul>
-      <div className="side-nav__divider" />
-      <ul className="side-nav__main">
-        <li>
-          <a href="/">Action</a>
-        </li>
-        <li>
-          <a href="/">Animation</a>
-        </li>
-        <li>
-          <a href="/">Comedy</a>
-        </li>
-        <li>
-          <a href="/">Crime</a>
-        </li>
-        <li>
-          <a href="/">Documentaries</a>
-        </li>
-        <li>
-          <a href="/">Drama</a>
-        </li>
-        <li>
-          <a href="/">Fantasy & Sci-fi</a>
-        </li>
-        <li>
-          <a href="/">Horro</a>
-        </li>
-        <li>
-          <a href="/">International</a>
-        </li>
-        <li>
-          <a href="/">Kids & Family</a>
-        </li>
-        <li>
-          <a href="/">Latino</a>
-        </li>
-        <li>
-          <a href="/">Music</a>
-        </li>
-        <li>
-          <a href="/">News/Talk</a>
-        </li>
-        <li>
-          <a href="/">Reality</a>
-        </li>
-        <li>
-          <a href="/">Romance</a>
-        </li>
-        <li>
-          <a href="/">Shorts</a>
-        </li>
-        <li>
-          <a href="/">Sports</a>
-        </li>
-        <li>
-          <a href="/">Suspense</a>
+        <li onClick={() => globalState.setSideNavOpenAction(false)}>
+          <Link href="/tv">
+            <a>
+              Series
+            </a>
+          </Link>
         </li>
       </ul>
     </div>
